de9c056be296dffec269d5ad0653b0376068fca4
William Thorsen <william@thorsen.io>
2019-02-08 18:47:27 -0700

    When values are provided in the variables files, don't compute them

--- src/compute-variables.sh
+++ src/compute-variables.sh
@@ -3,8 +3,6 @@
 # This script gets the values of regional and project-specific variables and uses them to generate
 # default values for other variables.
 
-# TODO: Don't compute a value if one is provided in one of the source files.
-
 # Change to the script's directory so that the variables files can be located by relative path,
 # then switch back after the variables files have been sourced
 THIS_SCRIPT_DIR=$(dirname $(realpath ${PWD}/${BASH_SOURCE[0]}))
@@ -24,7 +22,7 @@ SiteDomainName=${SiteDomainName:='www.example.com'}
 # (if they do not already exist)
 
 # Name of the S3 bucket that holds CloudFormation templates for the region
-TemplateBucketName="cf-templates-${AccountName}-${Region//-/}"
+TemplateBucketName="${TemplateBucketName:=cf-templates-${AccountName}-${Region//-/}}"
 
 
 # ----- Computed cluster variables for the project
@@ -32,14 +30,14 @@ TemplateBucketName="cf-templates-${AccountName}-${Region//-/}"
 EcsClusterName="${EcsClusterName:-${ProjectName}-cluster}"
 
 # These resources are shared by the cluster, so there should be only one of each
-BastionInstanceName="${EcsClusterName}-bastion"
-BastionStackName="${EcsClusterName}-bastion-stack"
-EcsStackName="${EcsClusterName}-stack"
-KeyPairKeyName="${EcsClusterName}-${Region//-/}"
+BastionInstanceName="${BastionInstanceName:=${EcsClusterName}-bastion}"
+BastionStackName="${BastionStackName:=${EcsClusterName}-bastion-stack}"
+EcsStackName="${EcsStackName:=${EcsClusterName}-stack}"
+KeyPairKeyName="${KeyPairKeyName:=${EcsClusterName}-${Region//-/}}"
 
 # TODO: Build in support for per-project subnets
-VpcDefaultSecurityGroupName="${EcsClusterName}-sg"
-VpcStackName="${EcsClusterName}-vpc-stack"
+VpcDefaultSecurityGroupName="${VpcDefaultSecurityGroupName:=${EcsClusterName}-sg}"
+VpcStackName="${VpcStackName:=${EcsClusterName}-vpc-stack}"
 
 # ----- Other computed project variables
 
@@ -53,11 +51,11 @@ BucketRandomSuffix=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 13 | head -n 1
 SiteBucketName="${SiteBucketName:=${SiteDomainName}-${BucketRandomSuffix}}"
 
 # This stack name is ignored if the S3 bucket stack is created as a nested stack
-SiteBucketStackName="${SiteBucketName//./-}-bucket-stack"
+SiteBucketStackName="${SiteBucketStackName:=${SiteBucketName//./-}-bucket-stack}"
 
 # Name of the index and error documents for the site (for an SPA, these are typically the same)
-SiteIndexDocument='index.html'
-SiteErrorDocument="${SiteIndexDocument}"
+SiteIndexDocument="${SiteIndexDocument:='index.html'}"
+SiteErrorDocument="${SiteErrorDocument:=${SiteIndexDocument}}"
 
 # --- CloudFront distribution
 
