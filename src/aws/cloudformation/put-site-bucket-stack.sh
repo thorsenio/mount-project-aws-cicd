12b5de093eda7d4be07925dda2b7b8ec3c31811f
William Thorsen <william@thorsen.io>
2019-02-08 18:31:19 -0700

    Adapt imported code
    Cicd-94 done

--- src/aws/cloudformation/put-site-bucket-stack.sh
+++ src/aws/cloudformation/put-site-bucket-stack.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
-TEMPLATE_FILE='./site-bucket.yml'
+# This script uses CloudFormation to create an S3 bucket to host the project's website files.
+
+# Typically, this script should be used only to test the template. Ordinarily, the bucket stack
+# is created as a nested stack within the S3 site stack.
+
+# There is no `delete-site-bucket-stack.sh`, because the bucket & stack get unique names
+# and these are not stored anywhere, so the deletion script would have no way of knowing which
+# bucket to delete.
+
+CLOUDFORMATION_TEMPLATE='templates/site-bucket.yml'
 
 # Change to the directory of this script
 cd $(dirname "$0")
 
-source functions.sh
-source variables-computed.sh
+source ../aws-functions.sh
+source ../../compute-variables.sh
 
 # Skip creation of the bucket if it already exists
 bucketExists ${PROFILE} ${SiteBucketName}
@@ -23,7 +32,7 @@ OUTPUT=$(aws cloudformation ${PUT_MODE}-stack \
   --profile ${PROFILE} \
   --region ${Region} \
   --stack-name ${SiteBucketStackName} \
-  --template-body file://${TEMPLATE_FILE} \
+  --template-body file://${CLOUDFORMATION_TEMPLATE} \
   --parameters \
     ParameterKey=SiteErrorDocument,ParameterValue=${SiteErrorDocument} \
     ParameterKey=SiteIndexDocument,ParameterValue=${SiteIndexDocument} \
