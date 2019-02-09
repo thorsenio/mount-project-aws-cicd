1c2a17833bf64edd51c2f3bcf3bbbecfc7b92a5f
William Thorsen <william@thorsen.io>
2019-02-08 21:18:24 -0700

    Import files
    Cicd-97 in progress

---
+++ src/aws/cloudformation/put-codebuild-project-stack.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# This script creates a CodeBuild project and related resources as a CloudFormation stack
+
+TEMPLATE_FILE='./codebuild-project.yml'
+
+# Change to the directory of this script
+cd $(dirname "$0")
+
+source functions.sh
+source variables-computed.sh
+
+# Capture the mode that should be used put the stack: `create` or `update`
+PUT_MODE=$(echoPutStackMode ${PROFILE} ${Region} ${CodeBuildProjectStackName})
+
+# TODO: REFACTOR: Use a function to generate ParameterKey,ParameterValue strings
+
+OUTPUT=$(aws cloudformation ${PUT_MODE}-stack \
+  --profile ${PROFILE} \
+  --region ${Region} \
+  --stack-name ${CodeBuildProjectStackName} \
+  --template-body file://${TEMPLATE_FILE} \
+  --parameters \
+    ParameterKey=CodeBuildProjectName,ParameterValue=${CodeBuildProjectName} \
+    ParameterKey=CodeBuildArtifactBucketName,ParameterValue=${CodeBuildArtifactBucketName} \
+    ParameterKey=CodePipelineArtifactBucketName,ParameterValue=${CodePipelineArtifactBucketName} \
+    ParameterKey=ProjectDescription,ParameterValue="${ProjectDescription}" \
+    ParameterKey=ProjectName,ParameterValue=${ProjectName} \
+    ParameterKey=RepoName,ParameterValue=${RepoName} \
+    ParameterKey=SiteBucketName,ParameterValue=${SiteBucketName} \
+  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+)
+
+EXIT_STATUS=$?
+echoPutStackOutput ${PUT_MODE} ${Region} ${EXIT_STATUS} ${OUTPUT}
