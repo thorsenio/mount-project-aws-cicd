225266f1b607acc923227c2d69701dfe64f17e4e
William Thorsen <william@thorsen.io>
2019-02-17 11:15:59 -0700

    Use DeploymentId (project resources) & PlatformId (shared resources)

--- src/compute-variables.sh
+++ src/compute-variables.sh
@@ -11,55 +11,76 @@ source ./config/regional-variables.sh
 source ./config/project-variables.sh
 cd - > /dev/null
 
-# ----- Dummy values for required variables
 # TODO: Verify that all required values exist
+
+# Platform deployment ID
+# The Dockerfile sets `PLATFORM_NAME`, `PLATFORM_VERSION` & `PLATFORM_VERSION_POSTFIX` as env vars
+PlatformName=${PLATFORM_NAME:='aws-cicd'}
+PlatformVersion=${PLATFORM_VERSION:='1.0.0'}
+PlatformVersionPostfix=${PLATFORM_VERSION_POSTFIX:=''}
+PlatformMajorVersion=$(echo ${PlatformVersion} | head -n 1 | cut -d . -f 1)
+PlatformId="${PlatformName}-p${PlatformMajorVersion}${PlatformVersionPostfix}"
+
+# Application deployment ID
+BranchName=${BranchName:='master'}
+if [[ ${BranchName} == 'master' ]]
+then
+  VersionPostfix=''
+else
+  # Use a deployment name if provided; otherwise, use the branch name (converting `/` to `-`)
+  VersionPostfix=${DeploymentName:=${BranchName//\//-}}
+fi
+
+# Project, version, and branch are combined into a value usable by the project
 ProjectDescription="${ProjectDescription:=${ProjectName}}"
 ProjectVersion="${ProjectVersion:=0.0.1}"
 ProjectMajorVersion=$(echo ${ProjectVersion} | head -n 1 | cut -d . -f 1)
+ProjectVersionBranch="${ProjectName}-v${ProjectMajorVersion}-${BranchName}"
+DeploymentId="${ProjectName}-v${ProjectMajorVersion}${VersionPostfix}"
 
-BranchName=${BranchName:=master}
-
-# TODO: FEATURE: Possibly add SiteUrl to allow for microservices hosted at the
-#  same domain
 SiteDomainName=${SiteDomainName:='www.example.com'}
+# TODO: FEATURE: Possibly add SiteUrl to allow for microservices hosted at the same domain
 # TODO: FEATURE: Support multiple domain names
 # TODO: FEATURE: Support URLs instead of domain names
-
-ProjectVersion="${ProjectVersion:=0.0.1}"
-ProjectMajorVersion=$(echo ${ProjectVersion} | head -n 1 | cut -d . -f 1)
-
-# Combine project, branch, and major version into a single value that can be used in resource names
-ProjectBranchVersion="${ProjectName}-${BranchName}-v${ProjectMajorVersion}"
 
 # ----- Defaults
 ProtectAgainstTermination='false'
 
+
 # Note that stack names generated below are ignored when the stacks are created as nested stacks
-# Typically, they would be created independently of their parent only during testing & development
+# Typically, they are created independently of their parent only during testing & development
 
-# ----- Computed regional variables
-
-# The S3 buckets below are referenced by the pipeline stack, but must be created independently
-# if they do not already exist
-
-# Name of the S3 bucket that hosts CodeBuild & CodePipeline artifacts for all projects in the region
-# Here they are configured to share a bucket
-CodeBuildArtifactBucketName="${CodeBuildArtifactBucketName:=cicd-artifacts-${AccountName}-${Region//-/}}"
-CodePipelineArtifactBucketName="${CodePipelineArtifactBucketName:=cicd-artifacts-${AccountName}-${Region//-/}}"
-CodeBuildServiceRoleName=${CodeBuildServiceRoleName:="cb-service-role-${ProjectBranchVersion}-${Region//-/}"}
-CodeBuildServiceRolePolicyName=${CodeBuildServiceRolePolicyName:="cb-service-role-policy-${ProjectBranchVersion}-${Region//-/}"}
+# ----- Account-wide variables
 
 # Name and ARN of the service role used by CodePipeline to call AWS services
-CodePipelineServiceRoleName=${CodePipelineServiceRoleName:='cp-service-role'}
+CodePipelineServiceRoleName=${CodePipelineServiceRoleName:="cp-service-role-${PlatformId}"}
 CodePipelineServiceRoleArn="arn:aws:iam::${AccountNumber}:role/${CodePipelineServiceRoleName}"
 
-# Name of the S3 bucket that holds CloudFormation templates for the region
-TemplateBucketName="${TemplateBucketName:=cf-templates-${AccountName}-${Region//-/}}"
+
+# ----- Region-wide variables
+
+# The S3 buckets below are referenced by the pipeline stack, but must be created independently if
+# they do not already exist. Use `create-cicd-artifacts-bucket.sh`.
+# AccountName is included in all bucket names to avoid name collisions
+
+# Name of the S3 bucket that hosts CodeBuild & CodePipeline artifacts for all projects in the region
+# Here CodeBuild & CodePipeline share the same bucket
+CodeBuildArtifactBucketName="${CodeBuildArtifactBucketName:=cicd-artifacts-${AccountName}-${PlatformId}-${Region//-/}}"
+CodePipelineArtifactBucketName="${CodePipelineArtifactBucketName:=cicd-artifacts-${AccountName}-${PlatformId}-${Region//-/}}"
+
+# Name of the service role & policy used by CodeBuild to call AWS services for this project
+CodeBuildServiceRoleName=${CodeBuildServiceRoleName:="cb-service-role-${PlatformId}-${Region//-/}"}
+CodeBuildServiceRolePolicyName=${CodeBuildServiceRolePolicyName:="cb-service-role-policy-${PlatformId}-${Region//-/}"}
+
+# Name of the S3 bucket that holds CloudFormation templates for the region. It must be created
+# ndependently if it does not already exist. Use `create-cf-templates-bucket.sh`
+TemplateBucketName="${TemplateBucketName:=cf-templates-${AccountName}-${PlatformId}-${Region//-/}}"
 
 
-# ----- Computed cluster variables for the project
+# ----- Cluster-wide variables
+
 # If the project specifies a cluster, it will be used; otherwise, the project gets its own cluster
-EcsClusterName="${EcsClusterName:=${ProjectBranchVersion}-cluster}"
+EcsClusterName="${EcsClusterName:=${DeploymentId}-cluster}"
 
 # These resources are shared by the cluster, so there should be only one of each
 BastionInstanceName="${BastionInstanceName:=${EcsClusterName}-bastion}"
@@ -68,19 +89,19 @@ EcsStackName="${EcsStackName:=${EcsClusterName}}"
 KeyPairKeyName="${KeyPairKeyName:=${EcsClusterName}-${Region//-/}}"
 
 # TODO: Build in support for per-project subnets
-VpcName="${VpcName:=${ProjectBranchVersion}-vpc}"
+VpcName="${VpcName:=${DeploymentId}-vpc}"
 VpcStackName="${VpcStackName:=${VpcName}}"
 VpcDefaultSecurityGroupName="${VpcDefaultSecurityGroupName:=${VpcName}-sg}"
 
-# ----- Other computed project variables
+# ----- Project-wide variables
 
 # --- CodeBuild project
-CodeBuildProjectName="${CodeBuildProjectName:=${ProjectBranchVersion}-cb-project}"
+CodeBuildProjectName="${CodeBuildProjectName:=${DeploymentId}-cb-project}"
 CodeBuildProjectStackName="${CodeBuildProjectStackName:=${CodeBuildProjectName}}"
 CodeBuildEnvironmentImage="${CodeBuildEnvironmentImage:='aws/codebuild/docker:18.09.0'}"
 
 # --- CodePipeline pipeline
-CodePipelineName="${CodePipelineName:=${ProjectBranchVersion}-cp}"
+CodePipelineName="${CodePipelineName:=${DeploymentId}-cp}"
 CodePipelineStackName="${CodePipelineStackName:=${CodePipelineName}}"
 
 # --- Events rule
@@ -97,10 +118,10 @@ RepoDescription="${RepoDescription:=${ProjectDescription}}"
 
 # --- Website stacks
 
-SiteStackName="${SiteStackName:=${ProjectBranchVersion}-site}"
+SiteStackName="${SiteStackName:=${DeploymentId}-site}"
 
 # The name and stack of the S3 bucket that hosts the project's static files
-ProjectBucketName="${ProjectBucketName:=${ProjectBranchVersion}-bucket}"
+ProjectBucketName="${ProjectBucketName:=${DeploymentId}-bucket}"
 ProjectBucketStackName="${ProjectBucketStackName:=${ProjectBucketName}}"
 
 # Name of the index and error documents for the site (for an SPA, these are typically the same)
@@ -108,4 +129,4 @@ SiteIndexDocument="${SiteIndexDocument:='index.html'}"
 SiteErrorDocument="${SiteErrorDocument:=${SiteIndexDocument}}"
 
 # --- CloudFront distribution
-CloudfrontDistributionStackName="${CloudfrontDistributionStackName:=${ProjectBranchVersion}-cdn}"
+CloudfrontDistributionStackName="${CloudfrontDistributionStackName:=${DeploymentId}-cdn}"
