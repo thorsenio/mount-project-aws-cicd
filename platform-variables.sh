72bb5ec17865218a339f92d3c7fba44732aeb3a1
William Thorsen <william@thorsen.io>
2019-02-17 22:51:27 -0700

    Add sample config files

--- platform-variables.sh
+++ platform-variables.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION='0.11.0'
+VERSION='0.12.0'
 ACCOUNT_NAME='skypilot'
 PACKAGE_NAME='aws-cicd'
 PROJECT_DIR="/var/lib/${PACKAGE_NAME}"
---
+++ src/config/project-variables.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# The values in this file overwrite the values declared in
+# - `global-variables.sh`
+# - `regional-variables.sh`
+# and the defaults computed in `compute-variables.sh`
+
+# Name of the project. This value is used to generate names for some of this project's resources
+ProjectName='my-project'
+ProjectDescription='My project'
+
+RepoName='my-project'
+RepoDescription='Description of my project'
+
+ProjectDomain='example.com'
+DevDomain='example.com'
+
+ProjectVersion='0.1.0'
