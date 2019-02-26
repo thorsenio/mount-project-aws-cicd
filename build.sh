2902737539a21239623296043304a73dca5ab58b
William Thorsen <william@thorsen.io>
2019-02-25 22:16:40 -0700

    Automatically determine version stage

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
 # This script builds the Docker image and tags it with the version information
-# contained in `platform-variables.sh`
-
-# Handle arguments
-if [[ $# -gt 1 ]]
-then
-  SHOW_USAGE='true'
-fi
+# contained in `platform-variables.sh` and derived from the current Git branch name
 
 # Change to the directory of this script so that relative paths resolve correctly
 cd $(dirname "$0")
@@ -35,46 +29,38 @@ fi
 
 IMAGE_NAME="${ACCOUNT_NAME}/${PACKAGE_NAME}"
 
-if [[ ${SHOW_USAGE} == 'true' ]]
-then
-  echo "Usage: ${0} [LABEL]"
-  echo "Examples:"
-  echo $(embold "  $0")
-  echo "  tags the image with these tags: $(embold "${IMAGE_NAME}:latest"), $(embold "${IMAGE_NAME}:${VERSION}")"
-  echo
-  echo $(embold "  $0 edge")
-  echo "  tags the image with these tags: $(embold "${IMAGE_NAME}:edge"), $(embold "${IMAGE_NAME}:edge-${VERSION}")"
-  exit 1
+echo "Version stage: ${VERSION_STAGE}"
+
+# If version stage is undefined, use the current Git branch
+if [[ -z ${VERSION_STAGE} ]]; then
+  BRANCH=$(git symbolic-ref --short HEAD)
+  VERSION_STAGE=${BRANCH//\//-}
 fi
 
-# Build the tag: label + version number
-if [[ $# -eq 1 ]]
-then
-  LABEL=$1
-  VERSION_PREFIX="${LABEL}-"
-  VERSION_STAGE=${LABEL}
-else
+# Build the tag: version number + version stage
+# Omit the version stage if this is the master version
+if [[ ${VERSION_STAGE} == 'master' ]]; then
   LABEL='latest'
-  VERSION_PREFIX=''
-  VERSION_STAGE=''
+  VERSION_LABEL=${VERSION}
+else
+  LABEL=${VERSION_STAGE}
+  VERSION_LABEL=${VERSION}-${VERSION_STAGE}
 fi
-
-TAG="${VERSION_PREFIX}${VERSION}"
-
-
-echo "Label: ${LABEL}"
-echo "Tag: ${TAG}"
 
 # Tag the build
 docker build -t ${IMAGE_NAME}:${LABEL} . \
+  --build-arg PACKAGE_NAME=${PACKAGE_NAME} \
   --build-arg VERSION=${VERSION} \
-  --build-arg VERSION_STAGE=${VERSION_STAGE} \
-  --build-arg PACKAGE_NAME=${PACKAGE_NAME}
+  --build-arg VERSION_STAGE=${VERSION_STAGE}
 
-if [[ ${?} -ne 0 ]]
+if [[ $? -ne 0 ]]
 then
   exit 1
 fi
 
 # Also tag the image with the version number, prefixed by the custom tag (if any)
-docker tag ${IMAGE_NAME}:${LABEL} ${IMAGE_NAME}:${TAG}
+docker tag ${IMAGE_NAME}:${LABEL} ${IMAGE_NAME}:${VERSION_LABEL}
+if [[ $? -ne 0 ]]; then
+  exit 1
+fi
+echo "Successfully tagged ${IMAGE_NAME}:${VERSION_LABEL}"
