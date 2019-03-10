123e4f0020bba1c6a4a7a978060d86f6b2329b19
William Thorsen <william@thorsen.io>
2019-03-10 11:52:43 -0600

    ssh-into-instance.sh: Default to 1st container instance

--- src/aws/ec2/ssh-into-ecs-instance.sh
+++ src/aws/ec2/ssh-into-ecs-instance.sh
@@ -7,13 +7,13 @@
 #   ssh-into-ecs-instance.sh 0
 #   ```
 
-if [[ ${#} -lt 1 ]]
+if [[ $# -gt 1 ]]
 then
-  echo "Usage: ${0} INSTANCE_INDEX" 1>&2
+  echo "Usage: $0 INSTANCE_INDEX" 1>&2
   exit 1
 fi
 
-INSTANCE_INDEX=${1}
+INSTANCE_INDEX=${1:-0}
 
 # Change to the directory of this script so that relative paths resolve correctly
 cd $(dirname "$0")
