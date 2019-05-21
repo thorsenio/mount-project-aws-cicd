974fd7a08da01e650cf30606d9b9a4736553e09e
William Thorsen <william@thorsen.io>
2019-05-21 12:28:58 -0600

    Reformat code

--- variables.sh
+++ variables.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-VERSION='2.5.0'
-VERSION_STAGE='master'
 DOCKER_ACCOUNT_NAME='skypilot'
 PACKAGE_NAME='mount-project-aws-cicd'
+
 PLATFORM_DIR="/var/lib/${PACKAGE_NAME}"
 PROJECT_DIR='/var/project'
+
+VERSION='2.5.0'
+VERSION_STAGE='master' # deprecated
